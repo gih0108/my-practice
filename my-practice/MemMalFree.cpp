83b73186fd370e6181696da6cd65c20a09d25162
JiHyun Lee <21600555@handong.edu>
2017-11-13 07:32:45 +0000

    yes

--- my-practice/MemMalFree.cpp
+++ my-practice/MemMalFree.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 char * MakeStrAdr(int len)
 {
-    char * str = (char*)malloc(sizeof(char)*len);
-    return str;
+    char * str1 = (char*)malloc(sizeof(char)*len);
+    char * str2 = (char*)malloc(sizeof(char)*len);
+    return str1;
+    return str2;
 }
 
 int main(void)
 {
-    char *str = MakeStrAdr(20);
-    strcpy(str, "I am so happy~");
-    cout << str << endl;
-    free(str);
+    char *str1 = MakeStrAdr(20);
+    char *str2 = MakeStrAdr(20);
+    strcpy(str1, "I am so happy~");
+    strcpy(str2, "I am so hungry~");
+    cout << str1 << endl;
+    cout << str2 << endl;
+    free(str1);
+
+    free(str2);
     return 0;
     
 }
