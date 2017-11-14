c9e1e24069c124fc4f08de5b65de911293e12e5c
JiHyun Lee <21600555@handong.edu>
2017-11-14 15:22:49 +0000

    cold

--- my-practice/MemMalFree.cpp
+++ my-practice/MemMalFree.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 char * MakeStrAdr(int len)
 {
-    char * str1 = (char*)malloc(sizeof(char)*len);
+    char * str1 = (char*)malloc(sizeof(char)*len); //len길이 만큼 공간 할당(malloc)
     char * str2 = (char*)malloc(sizeof(char)*len);
     return str1;
     return str2;
@@ -13,14 +13,13 @@ char * MakeStrAdr(int len)
 
 int main(void)
 {
-    char *str1 = MakeStrAdr(20);
-    char *str2 = MakeStrAdr(20);
+    char *str1 = MakeStrAdr(20); // 20만큼 공간 할당한 후 포인터 str1에 저장
+    char *str2 = MakeStrAdr(20); // 20만큼 공간 할당한 후 포인터 str2에 저장
     strcpy(str1, "I am so happy~");
     strcpy(str2, "I am so hungry~");
     cout << str1 << endl;
     cout << str2 << endl;
-    free(str1);
-
+    free(str1); // 삭제
     free(str2);
     return 0;
     
