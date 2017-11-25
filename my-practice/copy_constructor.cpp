b061eff6405bf1b016b9f688d79c242dba4d2a6c
JiHyun Lee <21600555@handong.edu>
2017-11-25 14:14:15 +0000

    lec8_copy cons

---
+++ my-practice/copy_constructor.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class point{
+    public:
+    int x,y;
+    
+    point(int_x, int_y){
+        x = _x; 
+        y = _y;
+    }
+    
+    point(const point &pt){
+        x = pt.x; 
+        y = pt.y;
+    }
+    
+    void set(int_x, int_y){
+        x = _x; 
+        y = _y;
+    }
+    void print();
+};
+
+void point::print(){
+    cout<<x<<","<<y<<endl;
+}
+
+int main(){
+    point p1(1,1); // p1.x = 1, p1.y = 1
+    point p2(p1);//p1객체 value 가져와서 초기화, p2 = p1
+    p1.set(2,2);// p1.x=2, p1.y=2 / p2.x=1, p2.y=1
+    cout << "P1:";
+    p1.print();
+    cout << "P2:";
+    p2.print();
+    
+    return 0;
+}
