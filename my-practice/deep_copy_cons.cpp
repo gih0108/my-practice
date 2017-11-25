1fce13c61b5801234a06dbe4ab5e63283cfef2c6
JiHyun Lee <21600555@handong.edu>
2017-11-25 14:59:11 +0000

    lec08_deep cons

--- my-practice/deep_copy_cons.cpp
+++ my-practice/deep_copy_cons.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
+#include <string.h> //strcpy, strlen 사용위해선 include 해줘야됨 
+
 using namespace std;
 
 class record{
     public:
     char* name;
-    char* tell;
+    char* tel;
     record(char*, char*);
-    record(const record &);
+    record(const record&);
     ~record();
-    void modify Tel(char* _tel);
+    void modifyTel(char* _tel);
     void print(void);
 };
 
@@ -16,11 +18,12 @@ record::record(char* _n, char* _tel)
 {
     name = new char[strlen(_n)+1];
     strcpy(name, _n);
-    tle = new char[strlen(_tel)+1];
-    strcpy(tel, _tel);
+    tel = new char[strlen(_tel)+1];
+    strcpy(tel, _tel); 
 }
 
-record::record(const record & _record){
+//deep copy
+record::record(const record& _record){
     name =new char[strlen(_record.name)+1];
     strcpy(name, _record.name);
     tel = new char[strlen(_record.tel)+1];
@@ -48,6 +51,6 @@ int main(){
     record hisRecord(myRecord);
     myRecord.modifyTel("3458");
     myRecord.print();
-    hisRecord.prin();
+    hisRecord.print();
     return 0;
 }
