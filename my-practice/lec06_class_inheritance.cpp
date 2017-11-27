da5f11f684e44737e20f497c34561ee67742bede
JiHyun Lee <21600555@handong.edu>
2017-11-27 10:49:09 +0000

    heritance_import

--- my-practice/lec06_class_inheritance.cpp
+++ my-practice/lec06_class_inheritance.cpp
@@ -8,14 +8,24 @@ class human{
     char* name;
     int age;
     char* hobby;
+    char* school;
 
     public:
-    human(char* _name,int _age,char* _hobby):name(_name),age(_age),hobby(_hobby){}//멤버 이니셜라이저
+    human(char* _name,int _age,char* _hobby,char * _school):name(_name),age(_age),hobby(_hobby),school(_school){}
+    //멤버 이니셜라이저, 상속받은 클래스이기 때문에 부모의 생성자 호출
+    //멤버 이니셜라이저를 반드시 써야하는 경우: 
+    //상수 멤버가 있을 때
+    //레퍼런스 멤버가 있을 때
+    //멤버의 생성자를 호출해야 할 때
+    //상속받은 클래스일 경우 부모의 생성자를 호출해야할 때
+    
     void Showinfo(){
-    std::cout<<name<<","<<age<<","<<hobby<<std::endl;}
+    std::cout<<name<<","<<age<<","<<hobby<<","<<school<<std::endl;
+        
+    }
     
     void Study(){
-        std::cout<<"공부"<<std::endl;
+        std::cout<<"공부해라"<<std::endl;
     }
     
 };
@@ -24,23 +34,23 @@ class student:public human{ //상속
     private:
         char school[30];
     public:
-    student(char* _name,int _age,char* _hobby,char* _school):human(_name,_age,_hobby){ //상속 
+    student(char* _name,int _age,char* _hobby,char* _school):human(_name,_age,_hobby,_school){ //상속 
         strcpy(school,_school);
     }
+    /*
     void Schoolinfo(){
         //Showinfo();
         std::cout<<school<<std::endl;
-    }
+    }*/
 };
 
 int main(){
-    student stu("이지현",21,"그림","한동");
-    stu.Schoolinfo();
+    student stu("이지현",21,"혼자놀기","한동");
+    //stu.Schoolinfo();
     stu.Showinfo();
     stu.Study();
 
     
     return 0;
 }
-    
     
