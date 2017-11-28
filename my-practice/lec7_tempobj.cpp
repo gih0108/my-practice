e9a7fb6a6cabaa07f9c87af30ecd8c7e3f985d90
JiHyun Lee <21600555@handong.edu>
2017-11-28 16:38:56 +0000

    temobj

--- my-practice/lec7_tempobj.cpp
+++ my-practice/lec7_tempobj.cpp
@@ -8,7 +8,7 @@ class Temporary
     public:
         Temporary(int n) : num(n)
         {
-            coun <<"create obj: " << num << endl;
+            cout <<"create obj: " << num << endl;
         }
 
         
@@ -25,14 +25,17 @@ class Temporary
 
 int main(void)
 {
-    Temporary(100);
+    Temporary(100);//임시객체 생성, 이 문장에 의해서 100으로 초기화된 Temporary 임시객체가 생성됨
     cout << "**********after make!" <<endl<<endl;
     
-    Temporary(200).ShowTempInfo(300);
+    Temporary(200).ShowTempInfo();//임시객체를 생성하고 이어서 이 객체를 대상으로 ShowTempInfo()함수를 호출하고 있음.
+    //객체가 생성 및 반환되려면 생성 및 반환된 위치에 객체를 참조할 수 있는 참조 값이 반환되기 때문에 이러한 문장을 구성할 수 있음
     cout << "**********after make!" <<endl<<endl;
     
-    const Temporary &ref=Temporary(300);
+    const Temporary &ref=Temporary(300);//임시객체 생성, 단 앞에서 생성한 다른 두 객체와 달리 여기서는 참조자 ref로 임시객체를 참조함
     cout << "**********end of main!" <<endl<<endl;
     return 0;
     
 }
+
+//임시객체는 다음 행으로 넘어가면 바로 소멸, 그러나 참조자에 참조되는 임시객체는 바로 소멸 안됨
