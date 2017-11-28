1578bf91dc7416d32abc222655e85a4ddb0bd632
JiHyun Lee <21600555@handong.edu>
2017-11-28 16:31:09 +0000

    temp

--- my-practice/lec7_objcopycons.cpp
+++ my-practice/lec7_objcopycons.cpp
@@ -22,8 +22,9 @@ class SoSimple
         }
 };
 
+//초기화의 대상은 obj각체가 아닌, ob객체, 그리고 ob객체는 obj객체로 초기화됨
 void SimpleFunObj(SoSimple ob)//매개변수 ob의 복사 생성자 호출, 인자로 obj가 전달될 것임
-//ob객체에는 SoSimple관련된 것들이 모두 들어올 것 임
+//ob객체에는 SoSimple관련된 것들이 모두 들어올 것임
 {
     ob.ShowData();
 }
