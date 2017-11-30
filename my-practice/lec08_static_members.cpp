3c263fef0107d09d34feb73afe00c314d012ef28
JiHyun Lee <21600555@handong.edu>
2017-11-30 09:10:32 +0000

    finished

--- my-practice/lec08_static_members.cpp
+++ my-practice/lec08_static_members.cpp
@@ -9,25 +9,25 @@ class math{
     static int facto;
     static int permu;
     
-    //이 부분 살짝 변형. 그래도 결과 값 출력되는데는 전혀 문제 없음.
+    /*이 부분 살짝 변형. 그래도 결과 값 출력되는데는 전혀 문제 없음.
     public:
     int sum1;
     int facto1;
     int permu1;
     
     math(){ //default constructor임을 주의!
-        sum = 0;
-        facto = 0;
-        permu = 0;
-    }
+        sum1 = 0;
+        facto1 = 0;
+        permu1 = 0;
+    }*/
     
     
-    /* 원래 public의 math함수 부분
+    //원래 public의 math함수 부분
     math(){ 
         sum = 0;
         facto = 0;
         permu = 0;
-    }*/
+    }
     
     //static member function들임!!
     static int summation(int a);
@@ -43,35 +43,35 @@ int math::permu = 1;//math class의 permu 변수가 1으로 초기화
 
 
 int math::summation(int a){
-    //sum = 0;
-    cout<<"sum"<< endl;
-    cout<<sum<< endl;
+    sum = 0;
+    //cout<<"sum"<< endl;
+    //cout<<sum<< endl;
     for(int i = 0; i<=a; i++)
         sum += i;
-    cout<<sum<< endl;
+    //cout<<sum<< endl;
     return sum;
 
 }
 
 int math::factorial(int a){
-    //facto = 1;
-    cout<<"fac"<< endl;
-    cout<<facto<< endl;
+    facto = 1;
+    //cout<<"facto"<< endl;
+    //cout<<facto<< endl;
     while(a!=0){
         facto  *= a;
         a-=1;
     }
-    cout<<facto<< endl;
+    //cout<<facto<< endl;
     return facto;
     
 }
 
 int math::permutation(int a, int b){
-    //permu = 1;
-    cout<<"permu"<< endl;
-    cout<<permu<< endl;
+    permu = 1;
+    //cout<<"permu"<< endl;
+    //cout<<permu<< endl;
     permu = math::factorial(a)/math::factorial(a-b);
-    cout<<permu<< endl;
+    //cout<<permu<< endl;
     return permu;
 }
 
@@ -86,9 +86,10 @@ int main(){
     cout<<"factorial:"<<result2<<endl;
     cout<<"permutation:"<<result3<<endl;
 
-    //임의로 만듦.
-    //math math1;//math class에 math1이라는 객체
-    //cout << "default constructor variable : " << math1.sum1 << ","<<math1.facto1<<","<<math1.permu1 << endl;
+    /*임의로 만듦.
+    math math1;//math class에 math1이라는 객체
+    cout << "default constructor variable : " << math1.sum1 << ","<<math1.facto1<<","<<math1.permu1 << endl;
+    */
 
     return 0;
 }
@@ -102,11 +103,10 @@ int main(){
 
 
 /*
-// line 82: 에서 출력을 math1.sum 이런식으로하면 sum 변수는 private 이기때문에 접근 불가능 해서 출력 불가함 
-// math()이 있는 이유는 생성자로서 private의 static int 변수를 초기화해주는것  
-//없애도 문제가없는건 line 39~ 41에서  static 변수를 한번더 초기화 해주기떄문에 그냥 돌아가는것
-너가 코드를 수정해서 정확히 어떤 부분에서 너가 의문을 느껴서 질문한 지는 모르겠는데 
-결국 코드가 보여주고자 하는 것은 static 변수를 초기화 하기 위해선 코드 외부에서
+math()이 있는 이유는 생성자로써 private의 static int 변수를 초기화 해주는것  
+없애도 문제가 되지 않는 것은 line 39~ 41에서  static 변수를 한번 더 초기화 해주기 때문에 그냥 돌아가는 것임
+
+결국 코드가 보여주고자 하는 것은 static 변수를 초기화 하기 위해서는 코드 외부에서
 int math::sum = 0; 같은 형태로 해야되는 것을 알려주고자 함 
 */
 
@@ -118,14 +118,18 @@ int math::sum = 0; 같은 형태로 해야되는 것을 알려주고자 함
 //permutation만 값이 0이됨. 결과가 달라짐.왜?
 
 
-/*static 변수의 특징은 같은 클래스 내에서 변수값이 공유가 되고 다른 함수에서 수정한 값이 그대로 남는다느
+/*static 변수의 특징은 같은 클래스 내에서 변수값이 공유가 되고 다른 함수에서 수정한 값이 그대로 남는다는
 특징을 갖고있다.
-또 초기화를 해준이유 : 앞에서 말했듯이 값이 남기 때문에 함수를 두번 불러쓰게 되면 ,
+함수의 기능을 해주는 코드에서 각 변수들을 또 초기화를 해준 이유는 
+앞에서 말했듯이 static 변수 특성 상 값이 남기 때문에 함수를 두 번 불러쓰게 되면 ,
 ex) facto(4) 결과가 15가 나왔는데 이 15가 facto 값에 남으면서 다음에 permu 계산할때 
 필요한 facto 값 1대신 15가 들어가게 됨 
-내가 원치않는 이전의 연산 결과 값이 입력값으로 들어가게 되기때문에 매번 함수를 불러낼떄마다
-변수들의 초기화 해줘야함
+내가 원치않는 이전의 연산 결과 값이 입력 값으로 들어가게 되 기때문에 매번 함수를 불러낼때마다
+변수들의 초기화 과정이 필요함.
 
 line 117 질문 : 앞에서 말헀듯이 함수내의 초기화를 지워버리면 이전 결과값으로 연산을 하기 떄문에
-17280 / 414720 이라는 연산을 하게됨 결과는 0.xxxx 겠지만 int 이므로 결과가 0이 출력 
+17280 / 414720 이라는 연산을 하게됨. 결과는 0.xxxx 겠지만 int 이므로 결과가 0이 출력될 것임
+
+따라서 코드에서 보여주고자 하는 것은 static 변수의 특성과 초기화를 통해 원래 원하던 값으로
+어떻게 출력하는지를 보여주는 것
 */
