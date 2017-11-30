070b0935b21d0d705e74b17147da945151c4be02
JiHyun Lee <21600555@handong.edu>
2017-11-30 16:56:57 +0000

    lec9

--- my-practice/lec9_functionoverride.cpp
+++ my-practice/lec9_functionoverride.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+
+
+//11,20,28행 총 3개의 클래스가 상속관계로 연결되어 있으며 이들은 모두 MyFunc 함수를 통해서 오버라이딩 관계를 형성
 class First
 {
 public:
@@ -28,10 +31,13 @@ void MyFunc(){
 
 int main(void)
 {
+    
+    //Third객체를 생성한 후, Third형, Second형, First형 포인터 변수로 이를 참조하고 있음
     Third * tptr = new Third();
     Second * sptr = tptr;
     First * fptr = sptr;
     
+    //각 포인터 형 변수를 이용해서 MyFunc 함수를 호출하고 있음
     fptr -> MyFunc();
     sptr -> MyFunc();
     tptr -> MyFunc();
