3fe1afa782791c1e464303e7b2efc42bc46ef09e
JiHyun Lee <21600555@handong.edu>
2017-11-30 18:31:08 +0000

    virtual..

--- my-practice/lec9_virtualDestructor.cpp
+++ my-practice/lec9_virtualDestructor.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
-class Fisrt
+class First
 {
 private:
     char *strOne;
 public:
     First(char * str)
     {
-        strone=new char[strlen(str)+1];
+        strOne=new char[strlen(str)+1];
+        cout<<str<<endl;
     }
-    ~First()
+    virtual ~First()
     {
         cout<<"~First()"<<endl;
         delete []strOne;
     }
 };
 
-class Second :public Fisrt
+class Second :public First
 {
 private:
     char * strTwo;
@@ -25,10 +27,20 @@ public:
     Second(char * str1, char * str2) : First(str1)
     {
         strTwo=new char[strlen(str2)+1];
+        cout<<str1<<","<<str2<<endl;
     }
-    ~Second()
+    virtual ~Second()
     {
         cout<<"~Second()"<<endl;
         delete []strTwo;
     }
+};
+
+int main(void)
+{
+    First * ptr = new Second("simple","complex");
+    //virtual이 선언되면 포인터의 자료형을 기반으로 호출대상을 결정하지 않고, 포인터 변수가 실제로 가르키는 객체를 호출대상으로 함
+    cout<<ptr<<endl;
+    delete ptr;
+    return 0;
 }
