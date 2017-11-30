f920d40220cdcc0a07e39eaceae2cb95142ac6a7
JiHyun Lee <21600555@handong.edu>
2017-11-30 17:02:00 +0000

    override

--- my-practice/lec9_functionoverride.cpp
+++ my-practice/lec9_functionoverride.cpp
@@ -38,9 +38,16 @@ int main(void)
     First * fptr = sptr;
     
     //각 포인터 형 변수를 이용해서 MyFunc 함수를 호출하고 있음
-    fptr -> MyFunc();
-    sptr -> MyFunc();
-    tptr -> MyFunc();
+    fptr -> MyFunc();//fptr이 First형 포인터이니, 이 포인터가 가리키는 객체를 대상으로 First 클래스에 정의된
+    //MyFunc함수는 무조건 호출할 수 있음
+    
+    sptr -> MyFunc();//sptr이 Second형 포인터이니, 이 포인터가 가리키는 객체에는 First의 MyFunc 함수와 Second의 MyFunc 함수가 
+    //오버라이딩 관계로 존재함, 때문에 오버라이딩을 한 Second의 MyFunc 함수를 호출할 것임
+    
+    tptr -> MyFunc();//컴파일러는 tptr이 Third형 포인터라는 사실을 기억, 따라서 이 포인터 변수가 참조하는 객체에는 총 세개의
+    //MyFunc 함수가 존재하고 이들은 오버라이팅 관계를 갖기 때문에 가장 마지막에 오버라이딩을 한 Third 클래스의 MyFunc함수가 호출되어야 함
+    
     delete tptr;
     return 0;
 }
+
