0533527b933e90498ce1904bf15db20dd7d7a41c
JiHyun Lee <21600555@handong.edu>
2017-12-01 15:46:59 +0000

    virtual

--- my-practice/lec9_virtualreference.cpp
+++ my-practice/lec9_virtualreference.cpp
@@ -51,12 +51,21 @@ int main(void)
     obj.ThirdFunc();
     obj.SimpleFunc();
     
-    Second & sref=obj;
-    sref.FirstFunc();
+    Second & sref=obj; //obj는 Second를 상속하는 Third객체이므로, Second형 참조자로 참조가 가능함
+    
+    //컴파일러는 참조자의 자료형을 가지고 함수의 호출 가능성을 판단하기 때문에 First클래스에 정의된
+    //FirstFunc 함수와 Second 클래스에 정의된 SecondFunc함수는 호출이 가능하지만,
+    //Third 클래스에 정의된 ThirdFunc함수는 호출이 불가능함
+    sref.FirstFunc(); 
     sref.SecondFunc();
+    
+    //SimpleFunc 함수는 가상함수이므로, Third 클래스에 정의된 SimpleFunc 함수가 호출됨
     sref.SimpleFunc();
     
-    First & fref=obj;
+    First & fref=obj;//obj는 First를 간접 상속하는 Third 객체이므로, First형 참조자로 참조 가능
+    
+    //컴파일러는 참조자의 자료형을 가지고 함수의 호출 가능성을 판단하기 때문에 First 클래스에 정의된 함수만 호출 가능
+    //그런데 이 중에서 SimpleFunc는 가상함수이므로, Third 클래스에 정의된 SimpleFunc 함수가 호출
     fref.FirstFunc();
     fref.SimpleFunc();
     return 0;
