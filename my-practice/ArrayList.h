96f3ec00c2333b2ef97f641c0f18d41895771119
JiHyun Lee <21600555@handong.edu>
2017-12-04 17:58:17 +0000

    header

--- my-practice/ArrayList.h
+++ my-practice/ArrayList.h
@@ -30,3 +30,7 @@ LData LRemove(List * plist);//참조한 데이터 삭제
 int LCount(List * plist);//저장된 데이터의 수 반환
 
 #endif
+
+//데이터의 저장공간이 배열로 선언되었고 저장된 데이터의 수를 기록하기 위한 멤버도 존재
+//참조의 위치를 기록하기 위한, LFirst와 LNext, 그리고 LRemove 함수를 위한 멤버도 존재
+//리스트에 다양한 종류의 데이터를 저장할 수 있게하기 위한 typedef 선언도 존재
