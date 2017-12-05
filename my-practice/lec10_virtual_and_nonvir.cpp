0a1ff73c2729a52909fe13bedbb4156210794ec9
JiHyun Lee <21600555@handong.edu>
2017-12-05 20:33:50 +0000

    lec10

---
+++ my-practice/lec10_virtual_and_nonvir.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Parent{
+public:
+    virtual void vpr()
+    {
+        cout <<"vpr.parent" <<endl;
+    }
+    void nvpr()
+    {
+        cout <<"nvpr.parent" <<endl;
+    }
+};
+
+class Child:public Parent{
+public:
+    void vpr()
+    {
+        cout<<"vpr.child"<<endl;
+        
+    }
+    void nvpr()
+    {
+        cout <<"nvpr.child"<<endl;
+    }
+};
+
+int main(void)
+{
+    Parent father;
+    Child son;
+    
+    Parent *par_pt = &son;
+}
