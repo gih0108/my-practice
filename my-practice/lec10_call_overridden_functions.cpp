db7a4b10b0524fedc8729977f9f61147e5cce9e3
JiHyun Lee <21600555@handong.edu>
2017-12-05 20:15:41 +0000

    lec10

---
+++ my-practice/lec10_call_overridden_functions.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Parent{
+public:
+    void print(){
+        cout << "I'm your father" <<endl;
+    }
+};
+
+class Child :public Parent{
+public:
+    void print(){
+        cout <<"I'm your son" <<endl;
+    }
+};
+
+int main({
+    Child child;
+    child.print();
+    child.Parent::print();
+    return 0;
+})
