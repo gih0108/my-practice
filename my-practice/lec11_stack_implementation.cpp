eb16378bdccc25c87bba37e8662efaefe57e171f
JiHyun Lee <21600555@handong.edu>
2017-12-08 15:05:39 +0000

    difficult

--- my-practice/lec11_stack_implementation.cpp
+++ my-practice/lec11_stack_implementation.cpp
@@ -2,72 +2,93 @@
 
 using namespace std;
 
-typedef struct
+class Element
 {
+public:
+    Element();
+public:
     int key;
+};
+
+class Stack
+{
+public:
+    Stack(int max_stack_size);
+    void Push(Element elem);
+    Element Pop();
+public:
+int max_size;
+int top;
+Element* stack;
+}
+
+Stack::Stack(int max_stack_size)
+:max_size(max_stack_size),top(-1),stack(NULL)
+{
+    stack = new Element[max_size];
+}
+
+Stack::Stack(const Stack& rhs)
+{
+    max_size = rhs.max_size;
+    top=rhs.top;
+    stack=new Element[max_size];
     
-}Element;
-
-typedef struct
-{
-    Element *stack;
-    int max_size;
-    int top;
-}Stack;
-
-Stack *CreateStack(int max_stack_size)
-{
-    Stack * s = (Stack*)malloc(sizeof(Stack));
-    s->stack = (Element*)malloc(max_stack_size*sizeof(Element));
-    s->max_size=max_stack_size;
-    s->top =-1;
-    return s;
-}
-
-Stack *DuplicateStack(Stack* original)
-{
-    Stack *s = CreateStack(original->max_size);
-    s->maz_size = original->max_size;
-    s->top = original->top;
-    for(int i=0;i<s->top+1; i++)
-        s->stack[i]=original->stack[i];
-    return s;
-}
-
-void Push(Stack *s, Element item)
-{
-    if(IsFullStack(s))
-        print("Stack Full\n");
-    else{
-        s->stack[++s->top] = item;
+    if(rhs.top>=0){
+        for(int i=0;i<top+1;i++)
+            stack[i]=rhs.stack[i];
     }
 }
 
-void DestroyStack(Stack *s)
+Stack& Stack::operator=(const Stack& rhs)
 {
-    free(s->stack);
-    free(s);
-    s = NULL;
+    if(&rhs != this){
+        max_size=rhs.max_size;
+        top=rhs.top;
+        stack = new Element[max_size];
+        
+        if(rhs.top>=0){
+            for(int i=0;i<top+1;i++)
+                stack[i]=rhs.stack[i];
+        }
+    }
+    return *this;
 }
 
-Element Pop(Stack *s)
+void Stack::Push(Element elem)
 {
-    if(IsEmptyStack(s))
-        print("Stack Empty\n");
+    if(IsFull())
+        cout << "Stack Full" <<endl;
     else{
-        return s->stack[s->top--];
+        stack[++top] =elem;
     }
 }
 
-int IsFullStack(Stack *s)
+Stack::~Stack()
 {
-    if(s->top==s->max_size-1)
-        return 1;
+    delete []stack;
+}
+
+Element Stack::Pop()
+{
+    if(IsEmpty()){
+        cout << "Stack Empty" <<endl;
+        return Element();
+    }
+    else{
+        return stack[top--];
+    }
+}
+
+bool IsFull()
+{
+    if(top==max_size-1)
+        return true;
     else
-        return 0;
+        return false;
 }
 
-int IsEmptyStack(Stack *s)
+bool IsEmptyStack(Stack *s)
 {
     if(s->top <0)
         return 1;
