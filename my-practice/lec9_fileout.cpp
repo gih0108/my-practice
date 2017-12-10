8a46116204135d0f5f684534dcef98c00e3c8c88
JiHyun Lee <21600555@handong.edu>
2017-12-10 14:10:38 +0000

    fileoutput

--- my-practice/lec9_fileout.cpp
+++ my-practice/lec9_fileout.cpp
@@ -1,13 +1,40 @@
 #include<iostream>
 #include <fstream>
+//#include <cstring>
+//#include <sstream>
+//#include <stdlib.h>
 
-int _tmain(int argc, _TCHAR* argv[])
+using namespace std;
+
+int iObj1;
+int iObj2;
+
+int main(void)
 {
-    ofstream f;
+    ofstream outFile;
     
-    f.open("cpptest.txt");
-    f<<"String"<<1234<<endl;
-    f.close();
+    outFile.open("textOut.txt");
+    iObj1 = 81;
+    outFile << "Obj1: " << iObj1 << endl;
+    iObj2 = iObj1 ++;
+    outFile << "iObj1: " << iObj1 << ","<< "iObj2: " << iObj2 << endl;
+    outFile.close(); 
     return 0;
 }
 
+
+
+ostream& operator<<(ostream &os, const IntClass &iObj1)
+{
+    os<<iObj1.getVal();
+    return(os);
+}
+
+int main(void)
+{
+    iObj1 = 81;
+    cout <<  "iObj1: " << iObj1 <<endl;
+    
+    iObj2 = iObj1 ++;
+    cout <<  "iObj1: " << iObj1 << ","<< "iObj2: " << iObj2 << endl;
+}
